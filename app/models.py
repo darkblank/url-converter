a1a72b7602a642b5061059d2980fda18c2a5d108
darkblank <darkblank1990@gmail.com>
2019-07-30 12:17:31 +0900

    random_url 만드는 function 구현

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 import datetime
+import string
+from random import choice
 
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
@@ -17,3 +19,8 @@ class Url(Base):
     hit_count = Column(Integer, default=0)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
     last_used_at = Column(DateTime, nullable=True)
+
+    @staticmethod
+    def random_url(length):
+        chars = string.ascii_letters + string.digits + '-' + '_'
+        return ''.join(choice(chars) for x in range(length))
