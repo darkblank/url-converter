8c3584ed1105d82c806d8647e45e4dcdd653e463
darkblank <darkblank1990@gmail.com>
2019-07-30 15:12:04 +0900

    shorten url 만드는 알고리즘 변경

--- app/models.py
+++ app/models.py
@@ -15,12 +15,16 @@ class Url(Base):
 
     id = Column(Integer, primary_key=True)
     original_url = Column(String(255), nullable=False)
-    short_url = Column(String(30), nullable=False, unique=True)
+    short_url = Column(String(30), nullable=True, unique=True)
     hit_count = Column(Integer, default=0)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
     last_used_at = Column(DateTime, nullable=True)
 
-    @staticmethod
-    def random_url(length):
-        chars = string.ascii_letters + string.digits + '-' + '_'
-        return ''.join(choice(chars) for x in range(length))
+    def generate_unique_shorten_url(self):
+        chars = string.ascii_letters + string.digits
+        n = self.id
+        url = ''
+        while n:
+            n, r = divmod(n, len(chars))
+            url += chars[r]
+        return url
