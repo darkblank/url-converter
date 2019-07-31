524e7e175aa163921908a94abd3aa26f0fb7db01
darkblank <darkblank1990@gmail.com>
2019-08-01 06:42:29 +0900

    Url id type을 UUID로 변경, Datetime Column들에 timezone 옵션 추가

--- app/models.py
+++ app/models.py
@@ -2,9 +2,11 @@ import datetime
 import random
 import re
 import string
+import uuid
 
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import UUIDType
 
 __all__ = ['Base', 'Url']
 
@@ -14,12 +16,12 @@ Base = declarative_base()
 class Url(Base):
     __tablename__ = 'url'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(UUIDType, primary_key=True, default=uuid.uuid4)
     original_url = Column(String(255), nullable=False)
     short_url = Column(String(30), nullable=True, unique=True)
     hit_count = Column(Integer, default=0)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
-    last_used_at = Column(DateTime, nullable=True)
+    created_at = Column(DateTime(timezone=True), default=datetime.datetime.now)
+    last_used_at = Column(DateTime(timezone=True), nullable=True)
 
     @staticmethod
     def is_valid_url_format(url):
