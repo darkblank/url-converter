b03b30d4c65f23f2d4d5ea5271684d7fdd442074
darkblank <darkblank1990@gmail.com>
2019-08-04 05:10:18 +0900

    base.html 이용해서 index.html 만들고, create_short_url api 이용하여 url 생성 기능 구현

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ view = Blueprint('view', __name__)
 
 @view.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
 
 @view.route('/<string:short_url>')
