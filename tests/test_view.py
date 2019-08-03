69bab0f4344e2d348bded83eac336724d94df55c
darkblank <darkblank1990@gmail.com>
2019-08-03 12:45:43 +0900

    redirect_to_url view 404 error test

--- tests/test_view.py
+++ tests/test_view.py
@@ -25,3 +25,9 @@ def test_redirect_to_url(client, session, url):
     assert r.status_code == 302
     assert renew.hit_count == url.hit_count + 1
     assert renew.last_used_at != url.last_used_at
+
+
+def test_redirect_to_url_404(client):
+    r = client.get(f'/doesNotExist')
+    assert r.status_code == 404
+    assert r.json['error'] == 'Url does not exist'
